bd5de2ac89c7344eab5f8193bde766a893b1d2a5
Nate Stephens <nate.dev@fivefloral.com>
2022-05-15 11:30:15 -0600

    multi-stage Dockerfile

---
+++ 05_multi-stage-build/index.js
@@ -0,0 +1,34 @@
+const hapi = require('@hapi/hapi');
+
+async function start() {
+  const server = hapi.server({
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler() {
+      return { success: true };
+    },
+  });
+
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      transport: {
+        target: 'pino-pretty',
+      },
+    },
+  });
+
+  await server.start();
+
+  return server;
+}
+
+start().catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
